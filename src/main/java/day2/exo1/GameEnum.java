beb6ad579db578cbed6df7714882ae11a135789b
Hugo Hersemeule <36131543+Owydoo@users.noreply.github.com>
2022-12-02 17:26:50 +0100

    ✨ Day 2 exo 1 - Rock, Paper & Scissors

---
+++ src/main/java/day2/exo1/GameEnum.java
@@ -0,0 +1,5 @@
+package day2.exo1;
+
+public enum GameEnum {
+    ROCK, PAPER, SCISSORS
+}
---
+++ src/main/java/day2/exo1/Main.java
@@ -0,0 +1,46 @@
+package day2.exo1;
+
+import utils.Parsing;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static int win = 6;
+    public static int draw = 3;
+    public static int loss = 0;
+
+
+    private static List<Match> linesToMatches(List<String> lines) {
+        List<Match> matches = new ArrayList<>();
+        for (String line :
+                lines) {
+            String[] values = line.split(" ");
+            matches.add(new Match(values[0], new PlayerInput(values[1])));
+        }
+        return matches;
+    }
+
+    public static void main(String[] args) {
+        //https://adventofcode.com/2022/day/2
+//        String filename = "src/main/java/day2/inputs/input1.txt";
+        String filename = "src/main/java/day2/inputs/input2.txt";
+        System.out.println("hello");
+
+        List<String> lines = Parsing.parseTextFile(filename);
+
+        System.out.println("lines" + lines);
+        List<Match> matches = linesToMatches(lines);
+        System.out.println("matches : " + matches);
+
+        List<Integer> scores = matches.stream().map(Match::getMyScore).toList();
+        System.out.println("scores : " + scores);
+
+        Integer totalScore = scores.stream().reduce(0, Integer::sum);
+        System.out.println("total score : " + totalScore);
+
+    }
+}
