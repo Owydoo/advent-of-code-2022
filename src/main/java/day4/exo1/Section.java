1959a21fac1a24630ace7ace3cce68b3a073e735
Hugo <owydoo@gmail.com>
2022-12-04 22:11:33 +0100

    🐛 Day 4 exo 1 - Used a list instead of a map to correct bug

--- src/main/java/day4/exo1/Section.java
+++ src/main/java/day4/exo1/Section.java
@@ -8,19 +8,11 @@ import lombok.Data;
 public class Section {
     private int first, last;
 
-    public boolean oneSectionIncludedInTheOther(Section section) {
-//        if (this.first <= section.first && this.last >= section.last){
-//            return true;
-//        }
-
-        return (this.first <= section.first && this.last >= section.last) || (section.first <= this.first && section.last >= this.last);
+    public boolean containsNumber(int number) {
+        return (number >= this.getFirst() && number <= this.getLast());
     }
 
-//            1-1,2-80
-//            28-58,59-59
-//            35-36,36-36
-//            1-48,1-75
-//            1-5,14-99
-//            44-60,44-77
-//            97-97,6-98
+    public boolean contains(Section section) {
+        return (this.containsNumber(section.getFirst()) && this.containsNumber(section.getLast()));
+    }
 }
