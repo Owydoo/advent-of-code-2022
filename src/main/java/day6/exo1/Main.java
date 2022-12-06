56ea4cc1f4351df20b8663984629869846324044
Hugo <owydoo@gmail.com>
2022-12-06 22:59:39 +0100

    ✨ Day 6 exo 2 - find start-of-message marker

--- src/main/java/day6/exo1/Main.java
+++ src/main/java/day6/exo1/Main.java
@@ -7,43 +7,46 @@ import java.util.List;
 
 public class Main {
 
-    public static final int MAKER_LENGTH = 4;
+    public static final int SMALL_MAKER_LENGTH = 4;
+    public static final int BIG_MAKER_LENGTH = 14;
 
     public static void main(String[] args) {
+        String filenameTest = "src/main/java/day6/inputs/input1.txt";
         String filename = "src/main/java/day6/inputs/input2.txt";
 
-//        List<String> lines = Parsing.parseTextFile(filename);
-//        System.out.println(lines);
+        List<String> lines = Parsing.parseTextFile(filenameTest);
+        System.out.println(lines);
 
         String input = Parsing.parseInlineTextFileAsString(filename);
 
-        System.out.println(getFirstMakerPosition(input));
+        System.out.println("res exo 1 for test inputs : " + getMakerPositions(lines, SMALL_MAKER_LENGTH));
+        System.out.println("Actual res exo 1 : " + getFirstMakerPosition(input, SMALL_MAKER_LENGTH));
 
-//        List<Integer> firstMakerPositions = getMakerPositions(lines);
-//        System.out.println(firstMakerPositions);
+        System.out.println("res exo 2 for test inputs : " + getMakerPositions(lines, BIG_MAKER_LENGTH));
+        System.out.println("Actual res exo 2 : " + getFirstMakerPosition(input, BIG_MAKER_LENGTH));
+
     }
 
-    private static List<Integer> getMakerPositions(List<String> lines) {
+    private static List<Integer> getMakerPositions(List<String> lines, int makerLength) {
         List<Integer> res = new ArrayList<>();
-//        res.add(getFirstMakerPosition(lines.get(0)));
         for (String line : lines) {
-            res.add(getFirstMakerPosition(line));
+            res.add(getFirstMakerPosition(line, makerLength));
         }
         return res;
     }
 
-    private static Integer getFirstMakerPosition(String input) {
+    private static Integer getFirstMakerPosition(String input, int makerLength) {
         boolean isMaker;
-        for (int i = 0; i <= input.length() - MAKER_LENGTH; i++) {
-            String potentialMaker = input.substring(i, i + MAKER_LENGTH);
-            isMaker = checkPotentialMaker(potentialMaker);
-            if (isMaker) return i + MAKER_LENGTH;
+        for (int i = 0; i <= input.length() - makerLength; i++) {
+            String potentialMaker = input.substring(i, i + makerLength);
+            isMaker = checkPotentialMaker(potentialMaker, makerLength);
+            if (isMaker) return i + makerLength;
         }
         return 0;
     }
 
-    private static boolean checkPotentialMaker(String potentialMaker) {
-        for (int index = 0; index < MAKER_LENGTH; index++) {
+    private static boolean checkPotentialMaker(String potentialMaker, int makerLength) {
+        for (int index = 0; index < makerLength; index++) {
             String tempPotentialMaker = getStringWithoutCharacterAtIndex(potentialMaker, index);
             if (tempPotentialMaker.contains(String.valueOf(potentialMaker.charAt(index)))) {
                 return false;
