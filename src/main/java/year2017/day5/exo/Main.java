6facc5b5e9536f4c4861aa7eb6f20f0c79c6a0bc
Hugo Hersemeule <36131543+Owydoo@users.noreply.github.com>
2023-08-21 17:00:49 +0200

    🎨 used List.copyOf inside functions instead of Main

--- src/main/java/year2017/day5/exo/Main.java
+++ src/main/java/year2017/day5/exo/Main.java
@@ -13,27 +13,26 @@ public class Main {
         List<Integer> inputList = new ArrayList<>(
                 Parsing.parseTextFile(filename).stream().map(Integer::parseInt).toList()
         );
-        List<Integer> inputForExo2 = new ArrayList<>(List.copyOf(inputList));
-
-        System.out.println(inputList);
 
         int stepsCounterForExo1 = countStepsInInputListExo1(inputList);
         System.out.println("answer exo 1 : " + stepsCounterForExo1);
 
-        int stepsCounterForExo2 = countStepsInInputListExo2(inputForExo2);
+        int stepsCounterForExo2 = countStepsInInputListExo2(inputList);
         System.out.println("answer exo 2 : " + stepsCounterForExo2);
     }
 
     private static int countStepsInInputListExo2(List<Integer> inputList) {
+        List<Integer> listCopy = new ArrayList<>(List.copyOf(inputList));
+
         int currentIndex = 0;
         int stepsCounter = 0;
 
-        while (currentIndex >= 0 && currentIndex < inputList.size()) {
-            int currentValue = inputList.get(currentIndex);
+        while (currentIndex >= 0 && currentIndex < listCopy.size()) {
+            int currentValue = listCopy.get(currentIndex);
             if (currentValue >= 3){
-                inputList.set(currentIndex, currentValue - 1);
+                listCopy.set(currentIndex, currentValue - 1);
             } else {
-                inputList.set(currentIndex, currentValue + 1);
+                listCopy.set(currentIndex, currentValue + 1);
             }
             currentIndex += currentValue;
             stepsCounter++;
@@ -42,12 +41,14 @@ public class Main {
     }
 
     private static int countStepsInInputListExo1(List<Integer> inputList) {
+        List<Integer> listCopy = new ArrayList<>(List.copyOf(inputList));
+
         int currentIndex = 0;
         int stepsCounter = 0;
 
-        while (currentIndex >= 0 && currentIndex < inputList.size()) {
-            int currentValue = inputList.get(currentIndex);
-            inputList.set(currentIndex, currentValue + 1);
+        while (currentIndex >= 0 && currentIndex < listCopy.size()) {
+            int currentValue = listCopy.get(currentIndex);
+            listCopy.set(currentIndex, currentValue + 1);
             currentIndex += currentValue;
             stepsCounter++;
         }
