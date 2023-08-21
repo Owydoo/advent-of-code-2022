d274085627aa711a628da4c014f9de96e16b2a63
Hugo Hersemeule <36131543+Owydoo@users.noreply.github.com>
2023-08-21 15:17:06 +0200

    ✨ Day 5 year 2017 - exo 1 completely redone

--- src/main/java/year2017/day5/exo/Main.java
+++ src/main/java/year2017/day5/exo/Main.java
@@ -4,55 +4,35 @@ import utils.Parsing;
 
 import java.util.List;
 
-import static java.lang.Math.abs;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("test autre implem");
 
         String filename = "src/main/java/year2017/day5/inputs/input.txt";
 //        String filename = "src/main/java/year2017/day5/inputs/inputTest.txt";
 
-        List<String> inputList = Parsing.parseTextFile(filename);
+        List<Integer> inputList = new java.util.ArrayList<>(
+                Parsing.parseTextFile(filename).stream().map(Integer::parseInt).toList()
+        );
 
-        MyLinkedList<Integer> inputInstructions = new MyLinkedList<>();
-        for (String s : inputList) {
-            inputInstructions.add(Integer.parseInt(s));
+        System.out.println(inputList);
+
+        //tant que l'indice auquel je cherche est contenu dans la liste
+        //-- je vais à l'indice indiqué en valeur à l'indice courant
+        //-- j'ajoute un à la valeur de départ
+        //-- je compte les étapes dans un compteur
+
+        int currentIndex = 0;
+        int stepsCounter = 0;
+
+        while (currentIndex >= 0 && currentIndex < inputList.size()) {
+            int currentValue = inputList.get(currentIndex);
+            inputList.set(currentIndex, currentValue + 1);
+            currentIndex += currentValue;
+            stepsCounter++;
         }
 
-        System.out.println("input instructions before exo 1 : " + inputInstructions);
-
-//        int stepsNumberExo1 = computeInstructions(inputInstructions);
-        int stepsNumberExo1 = 0;
-        stepsNumberExo1 = computeInstructions(inputInstructions.getHead(), stepsNumberExo1);
-
-        System.out.println("answer exo 1 : " + stepsNumberExo1);
-        System.out.println("input instructions after exo 1 : " + inputInstructions);
+        System.out.println("answer exo 1 : " + stepsCounter);
 
     }
-
-    private static int computeInstructions(MyLinkedList<Integer>.Node head, int counter) {
-//        head.setValue(head.getValue() + 1);
-        return jump(head, head.getValue(), counter);
-    }
-
-    private static int jump(MyLinkedList<Integer>.Node node, int currentCounter, int counter) {
-        if (node == null) {
-            return counter;
-        } else if (currentCounter > 0) {
-            return jump(node.next, currentCounter - 1, counter);
-        } else if (currentCounter < 0) {
-            return jump(node.previous, currentCounter + 1, counter);
-        } else if (currentCounter == 0) {
-//            node.setValue(node.getValue() + 1);
-            int updatedValue = node.value + 1;
-            node.setValue(updatedValue);
-            return jump(node, node.getValue() - 1, counter + 1);
-        } else {
-            throw new IllegalArgumentException();
-        }
-
-
-    }
-
 }
