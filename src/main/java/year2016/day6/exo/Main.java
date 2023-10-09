382d576160f5e8d7cdf83f31b8b4ddcf57326f76
Hugo Hersemeule <36131543+Owydoo@users.noreply.github.com>
2023-10-09 11:28:46 +0200

    ✨ Day 6 year 2016 - exo 2 done

--- src/main/java/year2016/day6/exo/Main.java
+++ src/main/java/year2016/day6/exo/Main.java
@@ -15,6 +15,7 @@ public class Main {
         List<String> inputWords = Parsing.parseTextFile("src/main/java/year2016/day6/inputs/input.txt");
 
         StringBuilder answerExo1 = new StringBuilder();
+        StringBuilder answerExo2 = new StringBuilder();
         //boucler sur les positions
         for (int position = 0; position < inputWords.get(0).length(); position++) {
             //boucler sur les mots
@@ -27,8 +28,10 @@ public class Main {
             }
             //ajout du carac max dans une string
             answerExo1.append(Collections.max(nbCharAtPosition.entrySet(), Map.Entry.comparingByValue()).getKey());
+            answerExo2.append(Collections.min(nbCharAtPosition.entrySet(), Map.Entry.comparingByValue()).getKey());
         }
 
-        System.out.println(answerExo1);
+        System.out.println("answer exo 1 : " + answerExo1);
+        System.out.println("answer exo 2 : " + answerExo2);
     }
 }
